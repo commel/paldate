3121f0c52360cc7512ce73cbf246de0635f82dae
Bernd Ritter <commel@gmail.com>
2014-09-01 16:58:04 +0200

    Paldate

--- app/Paldate.hx
+++ app/Paldate.hx
@@ -9,7 +9,15 @@ class Paldate {
   }
   
   public function new() {
-    Lib.print("Hallo");    
+    var date = Date.now();
+    var year = (date.getFullYear() - 1800) * 12;
+    var month = date.getMonth();
+
+    var palYear = year + month;
+    var tmpl = new haxe.Template( sys.io.File.getContent("../web/index.html") );
+    var output = tmpl.execute( { palYear: palYear } );
+
+    Lib.print(output);    
   }
 
 }
